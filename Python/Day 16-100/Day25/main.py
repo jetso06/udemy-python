f82524b68a85d9e166e5be2ffc38bd52f925c183
jetso06 <jerindavis@hotmail.com>
2026-05-29 09:57:44 +0400

    ok

---
+++ Python/Day 16-100/Day25/main.py
@@ -0,0 +1,24 @@
+# import csv
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         print(row)
+#         temp = int(row[1])
+#         temperatures.append(temp)
+# print(temperatures)
+
+import pandas
+data = pandas.read_csv("weather_data.csv")
+#temp_list = data["temp"].to_list()
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0]
+F = (monday_temp * 1.8) + 32
+monday.update(monday.temp = monday_temp)
+print(monday_temp)
