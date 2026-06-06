4738d9920aa2ad0abf20a38a25f8a7f08eae362a
jetso06 <jerindavis@hotmail.com>
2026-06-06 11:52:09 +0400

    ok

--- Python/Day 16-100/Day25/main.py
+++ Python/Day 16-100/Day25/main.py
@@ -1,24 +1,54 @@
-# import csv
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         print(row)
-#         temp = int(row[1])
-#         temperatures.append(temp)
-# print(temperatures)
+# # # import csv
+# # #
+# # # with open("weather_data.csv", "r") as data_file:
+# # #     data = csv.reader(data_file)
+# # #     temperatures = []
+# # #     for row in data:
+# # #         if row[1] != "temp":
+# # #             temperatures.append(int(row[1]))
+# # #
+# # # print(temperatures)
+# #
+# # import pandas
+# #
+# # data = pandas.read_csv("weather_data.csv")
+# # tempe = data["temp"]
+# # # print(tempe)
+# # #
+# # # mean = tempe.mean()
+# # # print(mean)
+# # # print(data.temp.max())
+# # # print(data[data.temp == data.temp.max()])
+# # monday = data[data.day == "Monday"]
+# # mondaytemp = monday.temp
+# # F = mondaytemp * (9/5) +32
+# # print(F)
+# import pandas
+#
+# data_dict = {
+#     "students" : ["Amy", "James", "Angela"]
+#     "scores" : [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("data.csv")
+
 
 import pandas
-data = pandas.read_csv("weather_data.csv")
-#temp_list = data["temp"].to_list()
 
-# print(data["temp"].mean())
-# print(data["temp"].max())
-#
-# print(data[data.temp == data.temp.max()])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20260531.csv")
+gray = len(data[data["Primary Fur Color"] == "Gray"])
 
-monday = data[data.day == "Monday"]
-monday_temp = monday.temp[0]
-F = (monday_temp * 1.8) + 32
-#monday.update(monday.temp = monday_temp)
-print(F)
+red = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
+black = len(data[data["Primary Fur Color"] == "Black"])
+
+print(black, gray, red)
+
+data_frame = {
+    "Primary Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count" : [gray, red, black],
+}
+
+data = pandas.DataFrame(data_frame)
+data.to_csv("squirrel_count.csv")
